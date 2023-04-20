7e17afa4e75c70cc511f9f6c07c3129e77d66f8f
Delerhon <66553893+Delerhon@users.noreply.github.com>
2023-04-20 12:02:15 +0200

    switched to assert method

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -19,70 +19,54 @@ const puzzleStringUnsolvable    = '1.1..2.84..63.12.7.2..5.....9..1....8.2.3674.
 
 suite('Unit Tests', () => {
     test('Logic handles a valid puzzle string of 81 characters', () => {
-        expect(solver.validate(puzzleStringGood))
-            .is.true
+        assert.isTrue(solver.validate(puzzleStringGood))
     })
     
     test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', () => {
-        expect(solver.validate(puzzleStringBad))
-            .has.property('error', 'char')
+        assert.propertyVal(solver.validate(puzzleStringBad), 'error', 'char')
     })
 
     test('Logic handles a puzzle string that is not 81 characters in length', () => {
-        expect(solver.validate(puzzleStringTooShort))
-            .has.property('error', 'length')
+        assert.propertyVal(solver.validate(puzzleStringTooShort), 'error', 'length')
     })
 
     test('Logic handles a valid row placement', () => {
-        expect(solver.checkRowPlacement(puzzleStringGood, 2, 2, 4))
-            .is.true
+        assert.isTrue(solver.checkRowPlacement(puzzleStringGood, 2, 2, 4))
     })
 
     test('Logic handles an invalid row placement', () => {
-        expect(solver.checkRowPlacement(puzzleStringGood, 2, 2, 6))
-            .is.false
+        assert.isFalse(solver.checkRowPlacement(puzzleStringGood, 2, 2, 6))
     })
 
     test('Logic handles a valid column placement', () => {
-        expect(solver.checkColPlacement(puzzleStringGood, 9, 6, 5))
-            .is.true
+        assert.isTrue(solver.checkColPlacement(puzzleStringGood, 9, 6, 5))
     })
 
     test('Logic handles an invalid column placement', () => {
-        expect(solver.checkColPlacement(puzzleStringGood, 9, 6, 8))
-            .is.false
+        assert.isFalse(solver.checkColPlacement(puzzleStringGood, 9, 6, 8))
     })
     
     test('Logic handles a valid region (3x3 grid) placement', () => {
-        expect(solver.checkRegionPlacement(puzzleStringGood, 8, 2, 8))
-            .is.true
+        assert.isTrue(solver.checkRegionPlacement(puzzleStringGood, 8, 2, 8))
     })
 
     test('Logic handles an invalid region (3x3 grid) placement', () => {
-        expect(solver.checkRegionPlacement(puzzleStringGood, 8, 2, 6))
-            .is.false
+        assert.isFalse(solver.checkRegionPlacement(puzzleStringGood, 8, 2, 6))
     })
 
     test('Valid puzzle strings pass the solver', () => {
-        expect(solver.validate(puzzleStringGood))
-            .is.true
+        assert.isTrue(solver.validate(puzzleStringGood))
     })
 
     test('Invalid puzzle strings pass the solver', () => {
-        expect(solver.validate(puzzleStringUnsolvable))
-            .is.false
+        assert.isFalse(solver.validate(puzzleStringUnsolvable))
     })
 
     test('Solver returns the expected solution for an incomplete puzzle', () => {
-        expect(solver.solve(puzzles[0][0]))
-            .to.be.equal(puzzles[0][1])
-        expect(solver.solve(puzzles[1][0]))
-            .to.be.equal(puzzles[1][1])
-        expect(solver.solve(puzzles[2][0]))
-            .to.be.equal(puzzles[2][1])
-        expect(solver.solve(puzzles[3][0]))
-            .to.be.equal(puzzles[3][1])
-        expect(solver.solve(puzzles[4][0]))
-            .to.be.equal(puzzles[4][1])
+        assert.equal(solver.solve(puzzles[0][0]), puzzles[0][1])
+        assert.equal(solver.solve(puzzles[1][0]), puzzles[1][1])
+        assert.equal(solver.solve(puzzles[2][0]), puzzles[2][1])
+        assert.equal(solver.solve(puzzles[3][0]), puzzles[3][1])
+        assert.equal(solver.solve(puzzles[4][0]), puzzles[4][1])
     })
 });
